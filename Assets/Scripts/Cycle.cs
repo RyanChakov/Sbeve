ae49857ee4f55ea243b36f8a327e67ee84e209a7
RyanChakov <ryanchakov@gmail.com>
2019-11-06 12:17:23 -0500

    new sky

---
+++ Assets/Scripts/Cycle.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cycle : MonoBehaviour
+{
+    public Material sky1, sky2, sky3, sky4, sky5, sky6, sky7, sky8;
+    public Material[] sky;
+    public float cycleT = 16f, timer,slow;
+    public int timercount=0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer > cycleT)
+        {
+            RenderSettings.skybox = sky[timercount];
+            timercount++;
+            if(timercount==8)
+            {
+                timercount = 0;
+            }
+            timer = 0;
+        }
+        else
+        {
+           
+            sky[timercount].SetFloat("_Blend", Mathf.Lerp(0, 1, Time.deltaTime));
+            print(sky[timercount].GetFloat("_Blend"));
+            timer += Time.deltaTime;
+        }
+    }
+}
--- Assets/Scripts/spawnMin.cs
+++ Assets/Scripts/spawnMin.cs
@@ -9,112 +9,112 @@ public class spawnMin : MonoBehaviour
     void Start()
     {
       
-            int xx = Random.Range(0, 200);
+            int xx = Random.Range(38, 230);
             int yy = Random.Range(0, 100);
-            int zz = Random.Range(0, 200);
+            int zz = Random.Range(30, 200);
             Instantiate(dungeon, new Vector3(xx, yy, zz), Quaternion.identity);
         
         for (int q = 0; q < 5; q++)
         {
-            int x = Random.Range(0, 200);
+            int x = Random.Range(0, 230);
            
             int z = Random.Range(0, 200);
             Instantiate(building1, new Vector3(x, 114, z), Quaternion.identity);
         }
         for (int q = 0; q < 10; q++)
         {
-            int x = Random.Range(0, 200);
+            int x = Random.Range(0, 230);
           
             int z = Random.Range(0, 200);
             Instantiate(building2, new Vector3(x, 116, z), Quaternion.identity);
         }
         for (int q = 0; q < 20; q++)
         {
-            int x = Random.Range(0, 200);
+            int x = Random.Range(0, 230);
            
             int z = Random.Range(0, 200);
             Instantiate(building3, new Vector3(x, 114, z), Quaternion.identity);
         }
         for (int q=0; q<50; q++)
         {
-            int x= Random.Range(0, 200);
+            int x= Random.Range(0, 230);
             int y = Random.Range(0, 50);
             int z = Random.Range(0, 200);
             Instantiate(gold, new Vector3(x, y, z), Quaternion.identity);
         }
         for (int q = 0; q < 100; q++)
         {
-            int x = Random.Range(0, 200);
+            int x = Random.Range(0, 230);
             int y = Random.Range(0, 75);
             int z = Random.Range(0, 200);
             Instantiate(amber, new Vector3(x, y, z), Quaternion.identity);
         }
         for (int q = 0; q < 100; q++)
         {
-            int x = Random.Range(0, 200);
+            int x = Random.Range(0, 230);
             int y = Random.Range(0, 75);
             int z = Random.Range(0, 200);
             Instantiate(amethyst, new Vector3(x, y, z), Quaternion.identity);
         }
         for (int q = 0; q < 100; q++)
         {
-            int x = Random.Range(0, 200);
+            int x = Random.Range(0, 230);
             int y = Random.Range(0, 75);
             int z = Random.Range(0, 200);
             Instantiate(aquamarine, new Vector3(x, y, z), Quaternion.identity);
         }
         for (int q = 0; q < 100; q++)
         {
-            int x = Random.Range(0, 200);
+            int x = Random.Range(0, 230);
             int y = Random.Range(0, 75);
             int z = Random.Range(0, 200);
             Instantiate(emerald, new Vector3(x, y, z), Quaternion.identity);
         }
         for (int q = 0; q < 100; q++)
         {
-            int x = Random.Range(0, 200);
+            int x = Random.Range(0, 230);
             int y = Random.Range(0, 75);
             int z = Random.Range(0, 200);
             Instantiate(garnet, new Vector3(x, y, z), Quaternion.identity);
         }
         for (int q = 0; q < 100; q++)
         {
-            int x = Random.Range(0, 200);
+            int x = Random.Range(0, 230);
             int y = Random.Range(0, 75);
             int z = Random.Range(0, 200);
             Instantiate(lapis, new Vector3(x, y, z), Quaternion.identity);
         }
         for (int q = 0; q < 100; q++)
         {
-            int x = Random.Range(0, 200);
+            int x = Random.Range(0, 230);
             int y = Random.Range(0, 75);
             int z = Random.Range(0, 200);
             Instantiate(obsidian, new Vector3(x, y, z), Quaternion.identity);
         }
         for (int q = 0; q < 100; q++)
         {
-            int x = Random.Range(0, 200);
+            int x = Random.Range(0, 230);
             int y = Random.Range(0, 75);
             int z = Random.Range(0, 200);
             Instantiate(pearl, new Vector3(x, y, z), Quaternion.identity);
         }
         for (int q = 0; q < 100; q++)
         {
-            int x = Random.Range(0, 200);
+            int x = Random.Range(0, 230);
             int y = Random.Range(0, 75);
             int z = Random.Range(0, 200);
             Instantiate(ruby, new Vector3(x, y, z), Quaternion.identity);
         }
         for (int q = 0; q < 100; q++)
         {
-            int x = Random.Range(0, 200);
+            int x = Random.Range(0, 230);
             int y = Random.Range(0, 75);
             int z = Random.Range(0, 200);
             Instantiate(smoky, new Vector3(x, y, z), Quaternion.identity);
         }
         for (int q = 0; q < 150; q++)
         {
-            int x = Random.Range(0, 200);
+            int x = Random.Range(0, 230);
             int y = Random.Range(0, 100);
             int z = Random.Range(0, 200);
             Instantiate(coal, new Vector3(x, y, z), Quaternion.identity);
