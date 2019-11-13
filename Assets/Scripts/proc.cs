8a2b17f61d2a5c9ac0144d15f065333bd5e835c1
RyanChakov <ryanchakov@gmail.com>
2019-11-13 13:15:08 -0500

    proc

--- Assets/Scripts/proc.cs
+++ Assets/Scripts/proc.cs
@@ -7,49 +7,38 @@ public class proc : MonoBehaviour
    
    
     public World world;
-    bool good = false;
     int chunkZ = 20;
     float newX = 0;
     float newZ = 0;
     //260 by 260
     void Update()
     {
+     
+
         if (!Physics.Raycast( transform.position,  transform.TransformDirection(Vector3.down)))
         {
-            good = true;
-
-        }
-        if (good)
-        { 
-            newX =  transform.position.x - ( transform.position.x % 20);
-            newZ =  transform.position.z - ( transform.position.z % 20);
+           if(transform.position.x < 0)
+            {
+                newX = transform.position.x - ((transform.position.x % 20)+20);
+            }
+            else
+            {
+                newX = transform.position.x - (transform.position.x % 20);
+            }
+            if (transform.position.z < 0)
+            {
+                newZ = transform.position.z - ((transform.position.z % 20)+20);
+            }
+            else
+            {
+                newZ = transform.position.z - (transform.position.z % 20);
+            }
             for (int y=0; y<13; y++)
                 CreateNewChunkNew((int)newX, y * 20, (int)newZ);
-            good = false;
+            print(newX + " X");
+            print(newZ + " Z");
         }
 
-    }
-   private void go()
-    {
-
-
-        newX =  transform.position.x - ( transform.position.x % 20);
-        newZ =  transform.position.z - ( transform.position.z % 20);
-        for (int y=0; y<13; y++)
-            CreateNewChunkNew((int)newX, y * 20, (int)newZ);
-        good = false;
-        /*for (int x = 0; x < 13; x++)
-        {
-            for (int y = 0; y < 13; y++)
-            {
-                for (int z = 0; z < 13; z++)
-                {
-                  
-                    CreateNewChunkNew((x * chunkZ)+(int)newX, (y * chunkZ), (z * chunkZ) + (int)newZ);
-                }
-            }
-        }*/
-       
     }
 
     private void CreateNewChunkNew(int x, int y, int z)
