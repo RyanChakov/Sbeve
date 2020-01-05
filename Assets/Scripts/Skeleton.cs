6768e381f986c0fd39f092fb12dd805213bb0fbf
RyanChakov <ryanchakov@gmail.com>
2020-01-05 18:43:24 -0500

    SkellyBoy

---
+++ Assets/Scripts/Skeleton.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skeleton : MonoBehaviour
+{
+
+  
+   
+  
+    public bool idle = false;
+   
+    public Animator Skel;
+  
+    public Transform target;
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Vector3.Distance(transform.position, target.position) >= 3f)
+        {
+            Skel.SetTrigger("running");
+        }
+        else if (Vector3.Distance(transform.position, target.position) < 3f)
+        {
+            Skel.SetTrigger("Attacking");
+            Skel.ResetTrigger("running");
+            attack();
+        }
+        
+    }
+    void attack()
+    {
+
+
+    }
+}
+
