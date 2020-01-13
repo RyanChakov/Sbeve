0c39b56d3ab1c91dc16a43ed0d7a293955ca1a57
RyanChakov <ryanchakov@gmail.com>
2020-01-13 12:17:57 -0500

    NEw

---
+++ Assets/Scripts/ChangeCamera.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeCamera : MonoBehaviour
+{
+    public bool dead;
+    public Camera mainCam;
+    public Camera deadCam;
+    public GameObject start, end;
+    public Transform player;
+    public int timer=0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if(dead)
+        {
+            mainCam.enabled = false;
+            deadCam.enabled = true;
+            //deadCam.transform.position = end.transform.position;
+
+            if(timer>1)
+            {
+                deadCam.transform.position = Vector3.Lerp(start.transform.position, end.transform.position, timer);
+            }
+            else
+            {
+                timer += Time.deltaTime;
+            }
+           
+            deadCam.transform.LookAt(player);
+           
+        }
+    }
+}
--- Assets/Scripts/Skeleton.cs
+++ Assets/Scripts/Skeleton.cs
@@ -8,17 +8,19 @@ public class Skeleton : MonoBehaviour
 {
     public moving player;
     bool idle = false;
-    float timerT = .75f;
+    float timerT = .78f;
     float timer = .78f;
     public Animator Skel;
+    public Animator Robot;
     public RectTransform hBar;
-    float bLength=.4f;
-    float bPosX = 204;
+    float bLength=.4f; 
     public Transform target;
+    public GameObject free;
+    public ChangeCamera cC;
     // Update is called once per frame
     void Update()
     {
-
+        
         if (Vector3.Distance(transform.position, target.position) >= 4f)
         {
             Skel.SetTrigger("running");
@@ -43,10 +45,22 @@ public class Skeleton : MonoBehaviour
             player.Phealth -= .1f;
             print("This is the player Health" + player.Phealth);
             timer = timerT;
-            bLength -= .04f;
-            bPosX -= 17.1f;
+            
+                bLength -= .04f;
+            if (bLength <= 0)
+            {
+                bLength = 0;
+                Robot.SetTrigger("Dead");
+                Robot.GetComponentInParent<moving>().enabled = false;
+                Robot.GetComponentInParent<CharacterController>().enabled = false;
+                free.GetComponent<FreeCam>().enabled = false;
+                cC.dead = true;
+
+            }
+          
+           
            hBar.localScale = new Vector3(bLength,.09f,1);
-            hBar.transform.position = new Vector3(bPosX, -38, 0);
+           
         }
         else
         {
