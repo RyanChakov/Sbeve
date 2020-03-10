d4b88914b4dd33fa41a3345357f090095067f897
RyanChakov <ryanchakov@gmail.com>
2020-03-10 13:12:30 -0400

    Intro Screen

---
+++ Assets/Scripts/RotateAround.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateAround : MonoBehaviour
+{
+    public Transform target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(target);
+        transform.Translate(Vector3.right * Time.deltaTime*5);
+    }
+}
